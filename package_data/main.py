fddde0dbee5b585b09cd5f538f2396a2980b6d05
Mr. Hallway <65696362+MrHallway0@users.noreply.github.com>
2020-12-09 12:11:59 -0800

    Update main.py

--- package_data/main.py
+++ package_data/main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+# Import modules that are needed
+import json, requests, sys, os
+
 # Select the info to get off of the file
-returnItems = ["Name", "Version", "About", "Developer", "Website", "Binary"]
+returnItems = ["Name", "Version", "About", "Developer", "Website"]
 
 # Set the package to install
-packageName = "package-example"
+packageName = str(sys.argv[1])
 
-# Import modules that are needed
-import json, requests
 
 # Prepare "repo.list"
 file = "../repo.list"
@@ -15,25 +16,58 @@ file = "../repo.list"
 # Open the file
 f = open(file, "r")
 repos = f.read().split()
+aRepoFound = False
 
 # Download data
 for url in repos:
     packageFile = url + packageName + "/manifest.json"
+    binary = url + packageName + "/install"
     r = requests.get(packageFile, allow_redirects=True)
     
     open("manifest.json", 'wb').write(r.content)
     
-    if str(r) == "<Response [404]>":
-        print(url +  " is an invalid repository or package.")
-
-# Open the json file
-jsonFile = open("manifest.json", "r")
-
-# Read the file
-f = json.load(jsonFile)
-
-# Return Values
-for returnItem in returnItems:
-	print(returnItem + ": " + f[returnItem]) 
-
-jsonFile.close()
+    # Pass if no package is found.
+    if str(r) == "<Response [404]>" and aRepoFound == False:
+        pass
+        
+    if str(r) == "<Response [200]>" and aRepoFound == False:
+        aRepoFound = True
+        # Open the json file
+        jsonFile = open("manifest.json", "r")
+        
+        # Read the file
+        f = json.load(jsonFile)
+        
+        # Return Values
+        for returnItem in returnItems:
+            print(returnItem + ": " + f[returnItem]) 
+        
+        jsonFile.close()
+        
+        # Add an extra line to make it easy to read
+        print()
+        
+        # Ask if the user wishes to install it and do so if they do.
+        while True:
+            yn = input("Would you like to install it? (y/n) ")
+            
+            if yn.lower() == "y":
+                break
+            
+            if yn.lower() == "n":
+                break
+            
+            else:
+                print()
+        
+        # Download & Install the package
+        if yn.lower() == "y":
+            print("Downloading " + binary)
+            r = requests.get(binary, allow_redirects=True)
+            open("install", 'wb').write(r.content)
+            os.system("chmod +x ./install && ./install")
+            
+        
+# If no package is found state that to the user
+if aRepoFound == False:
+    print("Package not found")
