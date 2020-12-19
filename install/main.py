a7de9ae7b7bcda31317185628c976902a42a2ad3
Mr. Hallway <65696362+MrHallway0@users.noreply.github.com>
2020-12-19 12:59:57 -0800

    Create main.py

---
+++ install/main.py
@@ -0,0 +1,15 @@
+import os
+
+# Make folders
+os.system("mkdir /apps")
+os.system("mkdir /apps/hop_files")
+
+# Copy HOP files
+os.system("cp ../repo.list /apps/hop_files")
+os.system("sudo cp ../package_data/main.py /bin/hop")
+os.system("sudo chmod +x /bin/hop")
+
+# Install modules
+os.system("python3 -m pip install rich")
+os.system("python3 -m pip install requests")
+os.system("python3 -m pip install json")
