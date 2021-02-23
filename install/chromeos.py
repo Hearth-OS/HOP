e253d811d744b40273c87be6e5f8900a2d00d669
Mr. Hallway <65696362+MrHallway0@users.noreply.github.com>
2021-02-23 11:37:34 -0800

    Update chromeos.py

--- install/chromeos.py
+++ install/chromeos.py
@@ -1,1 +1,16 @@
+import os
 
+print("[Warn] HOP for Chrome OS is in beta!")
+# Make folders
+os.system("mkdir /apps")
+os.system("mkdir /apps/hop_files")
+
+# Copy HOP files
+os.system("cp ../repo.list /apps/hop_files")
+os.system("sudo cp ../package_data/main.py /usr/local/bin/hop")
+os.system("sudo chmod +x /usr/local/bin/hop")
+
+# Install modules
+os.system("python3 -m pip install rich")
+os.system("python3 -m pip install requests")
+os.system("python3 -m pip install json")
